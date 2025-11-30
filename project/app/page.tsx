1a485896d25c79ed42cfb1ff4ca3080aa3aece80
Jay Lao <cjaylao447@gmail.com>
2025-11-30 20:41:20 +0800

    add:h2

--- project/app/page.tsx
+++ project/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
 <div>
-    <h1 class="">Welcome to the Home Page</h1>
+    <h1 >Welcome to the Home Page</h1>
 
     <h2>programming languages</h2>
 </div>
